c04f1172a4a8b65f6b2041aae9e9fc4b188f6fa9
Yustika Dyah Shafira <yustika.works@gmail.com>
2025-02-07 01:30:58 +0700

    ini adalah commit 3 branch-teman

--- data-teman3.js
+++ data-teman3.js
@@ -1,2 +1,3 @@
 let tempatTanggalLahir = ["Palu", 22, "Juli", 1998];
+
 console.log(tempatTanggalLahir);
--- data-teman4.js
+++
@@ -1,2 +0,0 @@
-let tempatTanggalLahir = ["Palu", 22, "Juli", 1998];
-console.log(tempatTanggalLahir);
