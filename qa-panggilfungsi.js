c719586901acb5c8f59fd0b8f2b7d0393c86a880
Yustika Dyah Shafira <yustika.works@gmail.com>
2025-02-07 00:01:57 +0700

    ini adalah contoh arrow function

---
+++ qa-panggilfungsi.js
@@ -0,0 +1,8 @@
+const panggilFungsi = (namaPanjang) => {
+  let tampilkan;
+  tampilkan = `Halooo, ${namaPanjang}`;
+
+  return tampilkan;
+};
+let nama = panggilFungsi("Yustika Dyah Shafira");
+console.log(nama);
